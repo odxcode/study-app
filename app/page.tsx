03c903fa6c1128a149292ad8f8f503e70c8de5e5
odxcode <owendail2006@gmail.com>
2026-08-22 23:24:26 -0400

    Redirect home page to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import Image from "next/image";
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+redirect('/login');
+}
 
 export default function Home() {
   return (
